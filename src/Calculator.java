142bcf6c0d079da01725b724ea91516f88149c85
Karel <improve-your-code@googlegroups.com>
2012-08-10 17:05:14 +1000

    Fixed div bug

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,8 @@
 public class Calculator {
     public int div(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("wrong argument");
+        }
         return a/b;
     }
 }
